ed9a3c54b197df79974557352a083ebcf1101a1a
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-09-16 22:17:03 +0300

    array-filter-method

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching (drivers,name) {
+    return drivers.filter (driver => driver===name);
+
+}
+
+function findMatching (drivers,name) {
+    return drivers.filter (driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch (drivers,searchString) {
+  const lowercaseSearch = searchString.toLowerCase();
+  return drivers.filter(driver => driver.toLowerCase().includes(lowercaseSearch));
+  
+}
+
+function fuzzyMatch(drivers, searchString) {
+  const lowercaseSearch = searchString.toLowerCase();
+  return drivers.filter(driver => driver.toLowerCase().startsWith(lowercaseSearch));
+}
+
+function matchName (drivers,nameToMatch) {
+    return drivers.filter(driver => driver.name === nameToMatch);
+}
